000859fbb34fbf48e17edc1e9f2e2e4a838baf6a
Adil <adil@Adils-MacBook-Air.local>
2026-02-02 17:56:18 +0530

    defg

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -12,7 +12,12 @@ module.exports = {
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PYTHONUNBUFFERED: '1'
+      PYTHONUNBUFFERED: '1',
+      GOOGLE_GEMINI_API_KEY: 'AIzaSyBpzW_w0s7BK0zfQb3KeP_uqgSUxBATh44',
+      MONGO_URL: 'mongodb+srv://moinmisba92:quickSell%40121@quicksellify.mdhrm.mongodb.net/quickSellify?retryWrites=true&w=majority&appName=quickSellify',
+      SECRET_KEY: 'your-secret-key-change-in-production-12345678',
+      SIGHTENGINE_API_USER: '144214407',
+      SIGHTENGINE_API_SECRET: 'JYA4RySafgQeKMUqnNGiQcdBFBuTKDk9'
     },
     error_file: '/root/backend/backend/logs/pm2-error.log',
     out_file: '/root/backend/backend/logs/pm2-out.log',
